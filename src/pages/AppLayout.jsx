25d7d4b51df87f97a8acdf7ce4f463faf091bc84
roman-pixel <romansrgvch@outlook.com>
2024-04-27 16:40:53 +0300

    add fake auth

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,12 +2,14 @@ import styles from "./AppLayout.module.css";
 
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
+import User from "../components/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
